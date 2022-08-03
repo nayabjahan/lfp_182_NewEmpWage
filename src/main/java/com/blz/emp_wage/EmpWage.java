bc9e4703e7ec2a409179cc11b01e8cb68b98320a
nayab jahan <nayabzafir@gmail.com>
2022-08-03 22:55:55 +0530

    Added:attendence

--- src/main/java/com/blz/emp_wage/EmpWage.java
+++ src/main/java/com/blz/emp_wage/EmpWage.java
@@ -3,5 +3,13 @@ package com.blz.emp_wage;
 public class EmpWage {
     public static void main(String[] args) {
         System.out.println("welcome to employee wage computation");
+        int emp_present = 1;
+        double emp_check = Math.floor(Math.random() * 10) % 2;
+        if (emp_check == emp_present) {
+            System.out.println("employee is present");
+        }
+        else {
+            System.out.println("employee is absent");
+        }
     }
 }
